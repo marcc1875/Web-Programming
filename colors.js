9aafe7ce026096bdf6c50ec490f88b2c5e7f77da
marcc1875 <144681076+marcc1875@users.noreply.github.com>
2023-09-28 09:19:26 +0100

    Version 2

--- colors.js
+++ colors.js
@@ -2,3 +2,20 @@ function getHue(range) {
 	var c = function () { return Math.floor(Math.random()*range); }
 	return "rgb(" + c() + "," + c() + "x" + c() + ")";
 }
+
+function getTable(size, range) {
+	var s = "<table>"
+	for (var i=0, i<size; i++) {
+		s += "<tr>";
+		for (var j=0; j<size; j++)
+		s += "<td style=\"background-color:" + getHue(range) +
+		"\">" + "     </td>";
+		s += "</tr>";
+	}
+	
+	s += "</table>";
+	return s;
+}
+
+var s = getTable(Math.floor(Math.random()*40 + 1), 256);
+document.writeln(s);
