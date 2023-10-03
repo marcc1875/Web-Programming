207c95aa34dd7af22b16a87ead5ed6b1c091918f
marcc1875 <144681076+marcc1875@users.noreply.github.com>
2023-10-03 18:28:58 +0100

    Version 2

--- edit.js
+++ edit.js
@@ -1,3 +1,12 @@
 function modify() {
   $("div").width("100pt");
 }
+
+$(document).ready(function(){
+  $("#hide").click(function(){
+    $("p").hide();
+  });
+  $("#show").click(function(){
+    $("p").show();
+  });
+});
