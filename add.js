de137b70bf0198e4e113d2cfe06f3815e6e58df4
marcc1875 <144681076+marcc1875@users.noreply.github.com>
2023-10-23 13:41:19 +0100

    Version 4

--- add.js
+++ add.js
@@ -13,3 +13,12 @@ $(document).ready(function(){
     $("#panel").slideDown("slow");
   });
 });
+
+function loadDoc() {
+  const xhttp = new XMLHttpRequest();
+  xhttp.onload = function() {
+    document.getElementById("demo").innerHTML = this.responseText;
+    }
+  xhttp.open("GET", "ajax_info.txt", true);
+  xhttp.send();
+}
